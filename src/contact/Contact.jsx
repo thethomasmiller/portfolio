9c40eb615f26a28c88bb75ed79415745014ec7a5
Thomas Miller <thomasmiller@Thomass-Air.home>
2020-08-25 17:08:09 -0400

    Contact Links

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Nav from '../nav/Nav'
+import gitHubIcon from '../assets/gitHubIcon.png'
+import liIcon from '../assets/liIcon.png'
 import './Contact.css'
 
 export default function Contact() {
   return (
     <div>
-      <Nav/>
+      <Nav />
+     <a href='https://www.linkedin.com/in/thethomasmiller/'> <img className='contact-li-icon' src={`${liIcon}`}/> </a>
+     <a href='https://github.com/thethomasmiller'> <img className='contact-github-icon' src={`${gitHubIcon}`}/> </a>
+
     </div>
   )
 }
